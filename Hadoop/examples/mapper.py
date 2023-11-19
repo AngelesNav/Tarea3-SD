8db066defbcccbe20d939414b1cdf56e1067800e
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-19 17:15:50 -0300

    Update mapper.py

--- Hadoop/examples/mapper.py
+++ Hadoop/examples/mapper.py
@@ -1,14 +1,21 @@
 import sys
 import re
 
+all_words = []  # Lista para almacenar todas las palabras del texto
+
 for line in sys.stdin:
-    line = re.sub(r'[^a-zA-ZñÑ\s]|[\d]', '', line.strip())
+    line = re.sub(r'[^a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]', '', line.strip())
     words = line.split()
 
-    # Obtener el nombre del archivo desde el nombre de la entrada
-    filename = sys.argv[1] 
+    # Agregar todas las palabras a la lista
+    all_words.extend(words)
 
-    for word in words:
-        # Excluir palabras vacías
-        if word:
-            print('{}\t{}\t{}'.format(filename, word, 1))
+# Obtener el nombre del archivo desde el nombre de la entrada
+filename = sys.argv[1]
+
+# Usar un conjunto para obtener palabras únicas y contar su frecuencia
+unique_word_set = set(all_words)
+for word in unique_word_set:
+    # Excluir palabras vacías
+    if word:
+        print('{}\t{}\t{}'.format(filename, word, all_words.count(word)))

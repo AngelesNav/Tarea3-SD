6b2e3ec0f05f65e893d0f0233eff0c017e87b3b4
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-19 17:16:18 -0300

    Update reducer.py

--- Hadoop/examples/reducer.py
+++ Hadoop/examples/reducer.py
@@ -3,17 +3,15 @@ import sys
 current_word = None
 current_count = 0
 word = None
-current_filename = None
 
 for line in sys.stdin:
     line = line.strip()
-    word, filename, count = line.split('\t', 2)
+    word, count = line.split('\t', 1)
 
     try:
         count = int(count)
     except ValueError:
         continue
-
 
     if current_word == word:
         current_count += count
@@ -23,7 +21,6 @@ for line in sys.stdin:
         current_word = word
         current_count = count
 
-# Imprimir el último grupo de palabras al final del archivo
-if current_word:
+if current_word == word:
     print('{}\t{}'.format(current_word, current_count))
 

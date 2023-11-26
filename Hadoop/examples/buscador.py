9894fc28f08394feb8eccbff50b3db68d8b635fb
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-26 19:56:20 -0300

    Update buscador.py

--- Hadoop/examples/buscador.py
+++ Hadoop/examples/buscador.py
@@ -1,28 +1,9 @@
 import re
 
-def buscar_en_archivo(consulta, archivo, diccionario_documentos):
-    # Leemos el contenido del archivo
-    with open(archivo, 'r', encoding='utf-8') as file:
-        contenido = file.read()
+# Ruta al archivo de datos
+ruta_archivo = '/workspaces/Tarea3-SD/Hadoop/examples/outhadoop/part-00000.txt'
 
-    # Utilizamos una expresión regular para encontrar todas las líneas que contienen la palabra de consulta
-    patrón = re.compile(f'{consulta}:.*?\n')
-    coincidencias = patrón.findall(contenido)
-
-    # Creamos una lista de tuplas con el número de documento y la frecuencia para cada coincidencia
-    resultados = [(int(match.group(1)), int(match.group(2))) for match in re.finditer(r'\((\d+), (\d+)\)', ''.join(coincidencias))]
-
-    # Ordenamos los resultados por frecuencia en orden descendente
-    resultados = sorted(resultados, key=lambda x: x[1], reverse=True)
-
-    # Tomamos los 5 números de documento con las frecuencias más altas
-    mejores_resultados = resultados[:5]
-
-    # Extraemos la dirección de Wikipedia para cada resultado
-    resultados_con_direccion = [(numero_documento, frecuencia, diccionario_documentos.get(numero_documento, f'Dirección de Wikipedia no encontrada para el documento {numero_documento}')) for numero_documento, frecuencia in mejores_resultados]
-
-    return resultados_con_direccion
-
+# Arreglo de entradas y diccionario de documentos
 entradas = [
     "Apple", "Samsung", "Google", "Microsoft", "Amazon",
     "Coca-Cola", "IBM", "Toyota", "McDonald's", "Disney",
@@ -34,17 +15,43 @@ entradas = [
 
 diccionario_documentos = {i+1: f'https://es.wikipedia.org/wiki/{entrada}' for i, entrada in enumerate(entradas)}
 
-# Ejemplo de uso
-consulta_usuario = input("Ingrese la palabra a buscar: ")
-archivo_a_buscar = '/workspaces/Tarea3-SD/Hadoop/examples/outhadoop/part-00000'  # Reemplaza 'archivo.txt' con el nombre de tu archivo
+# Procesar datos proporcionados
+def procesar_datos(datos_providenciados):
+    diccionario_resultados = {}
 
-# Realizamos la búsqueda
-resultados = buscar_en_archivo(consulta_usuario, archivo_a_buscar, diccionario_documentos)
+    lineas = datos_providenciados.strip().split('\n')
 
-if resultados:
-    print(f"\nLos 5 mejores resultados para '{consulta_usuario}' son:")
-    
-    for i, resultado in enumerate(resultados, 1):
-        print(f"\nDocumento: {resultado[0]}\nFrecuencia: {resultado[1]}\nURL: {resultado[2]}\n")
-else:
-    print(f"\nNo se encontraron resultados para '{consulta_usuario}'.")
+    for linea in lineas:
+        palabra, posiciones = linea.split(None, 1)
+        posiciones = re.findall(r'\((\d+), (\d+)\)', posiciones)
+
+        diccionario_resultados[palabra] = [(int(doc), int(freq)) for doc, freq in posiciones]
+
+    return diccionario_resultados
+
+# Función de búsqueda e impresión de los 5 con mayor frecuencia
+def buscar_e_imprimir(diccionario_resultados, diccionario_documentos, palabra):
+    resultados = diccionario_resultados.get(palabra, [])
+
+    if resultados:
+        # Ordenar los resultados por frecuencia en orden descendente
+        resultados_ordenados = sorted(resultados, key=lambda x: x[1], reverse=True)[:5]
+
+        print(f'\nResultados para la palabra "{palabra}":\n')
+        for doc, freq in resultados_ordenados:
+            url = diccionario_documentos.get(doc, f'URL no encontrada para el documento {doc}')
+            print(f'Documento: {doc}\nFrecuencia: {freq}\nURL: {url}')
+            print('\n')
+    else:
+        print(f'No se encontraron resultados para la palabra "{palabra}".')
+
+# Leer el archivo de datos
+with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+    datos_providenciados = archivo.read()
+
+# Solicitar al usuario ingresar una palabra clave
+palabra_clave = input("Ingrese la palabra clave a buscar: ").strip().lower()
+
+# Procesar datos y realizar búsqueda e impresión de los 5 con mayor frecuencia
+resultados_procesados = procesar_datos(datos_providenciados)
+buscar_e_imprimir(resultados_procesados, diccionario_documentos, palabra_clave)

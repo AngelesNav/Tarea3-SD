551f5829d2ec7fd8a1660d6046d80be2862be260
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-26 19:20:50 -0300

    Update wp.py

--- Hadoop/examples/wikipedia/wp.py
+++ Hadoop/examples/wikipedia/wp.py
@@ -2,38 +2,41 @@ import requests
 import json
 
 entradas = [
-    "Apple", "Samsung", "Google", "Microsoft", "Amazon",
-    "Coca-Cola", "IBM", "Toyota", "McDonald's", "Disney",
-    "Nike", "Adidas", "Pepsi", "Panasonic", "BMW",
-    "Mercedes-Benz", "Volkswagen", "Ford", "Chevrolet", "Prada",
-    "Nestlé", "Intel", "Honda", "LG", "Nikon",
-    "JBL", "Philips", "Lenovo", "Asus", "Salesforce"
+    "Sony", "Samsung", "LG", "Panasonic", "Philips",
+    "Sharp", "Toshiba", "Bose", "Apple", "Microsoft",
+    "Dell", "HP", "Lenovo", "Acer", "Asus",
+    "Canon", "Nikon", "Sony Ericsson", "Huawei", "Xiaomi",
+    "Google", "Amazon", "Olidata", "GoPro", "JBL",
+    "Bose", "Sennheiser", "Skullcandy", "Beats", "Bowers & Wilkins"
 ]
 
-url = "https://es.wikipedia.org/w/api.php" 
+url = "https://en.wikipedia.org/w/api.php"
 i = 1
-
 for entrada in entradas:
     params = {
         'format': 'json',
         'action': 'query',
         'prop': 'extracts',
+        'exintro': '',
         'explaintext': '',
         'redirects': 1,
         'titles': entrada
     }
 
-    req = requests.get(url, params=params).json()
+    req = requests.get(
+        url,
+        params=params
+    ).json()
 
-    # Manejo de la respuesta JSON
     n_page = list(req['query']['pages'].keys())[0]
     texto = req['query']['pages'][n_page]['extract']
+    texto = '{}<splittername>{}'.format(i, json.dumps(texto))
 
-    # Escribir el texto en un archivo
     if i <= 15:
-        with open(f'../carpeta1/{entrada}.txt', 'w', encoding='utf-8') as f:
+        with open(f'../carpeta1/{i}.txt', 'w') as f:
             f.write(texto)
     else:
-        with open(f'../carpeta2/{entrada}.txt', 'w', encoding='utf-8') as f:
+        with open(f'../carpeta2/{i}.txt', 'w') as f:
             f.write(texto)
-    i += 1
+    i = i + 1
+

f3391db746ebe86a5e2b9e5720a22673bf565dda
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-26 18:04:28 -0300

    Update contador.sh

--- Hadoop/examples/contador.sh
+++ Hadoop/examples/contador.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Archivo para almacenar todos los resultados
-RESULT_FILE="resultados_contador_simple.txt"
+# Archivo para almacenar todos los resultados de mapper
+RESULT_FILE="mapper.txt"
 
 # Limpiar o crear el archivo de resultados
 > $RESULT_FILE
 
-# Ejecutar el script mapper en todos los archivos de la carpeta 1
-for file in carpeta1/*.txt; do
-    filename=$(basename -- "$file")
-    echo "Procesando archivo: $filename"
-    cat $file | python mapper.py $filename >> $RESULT_FILE
-done
-
-# Ejecutar el script mapper en todos los archivos de la carpeta 2
-for file in carpeta2/*.txt; do
+# Ejecutar el script mapper en todos los archivos de las carpetas
+for file in carpeta1/*.txt carpeta2/*.txt; do
     filename=$(basename -- "$file")
     echo "Procesando archivo: $filename"
     cat $file | python mapper.py $filename >> $RESULT_FILE
@@ -22,6 +15,9 @@ done
 
 # Ejecutar el script reducer en el archivo de resultados
 cat $RESULT_FILE | sort | python reducer.py > resultado_final.txt
+
+echo "Resultados finales guardados en resultado_final.txt"
+
 
 echo "Resultados finales guardados en resultado_final.txt"
 
